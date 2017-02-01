4880e779d9e222fa15e0a54fdd162ee999d71fb5
adbooth <adbooth8@gmail.com>
2017-01-31 22:06:54 -0500

    Added DWUnique

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,11 @@
 /* index.js
-*/
+ */
 
 function change_scene(target_id){
-  var scenes = document.getElementsByClassName('scene');
-  for(var scene of scenes){
-    scene.style.display = 'none';
-  }
-  var target_scene = document.getElementById(target_id);
-  target_scene.style.display = 'block';
+    var scenes = document.getElementsByClassName('scene');
+    for(var counter = 0; counter < scenes.length; counter++){
+        scenes[counter].style.display = 'none';
+    }
+    var target_scene = document.getElementById(target_id);
+    target_scene.style.display = 'block';
 }
